c4fa4c9099c104afd1d3d998df89fbbd79fb812f
Andreas Schmidt <andreas.schmidt@cassini.de>
2014-03-03 22:20:03 +0100

    simplified properties

--- demosteps/50_properties/spec/node/501_facter_network_prop_spec.rb
+++ demosteps/50_properties/spec/node/501_facter_network_prop_spec.rb
@@ -1,10 +1,10 @@
 require 'spec_helper'
 
 describe network_fact('eth0') do
-	its(:cidr) 	{ should eq "#{property[:access_network]}" }
+	its(:cidr) 	{ should eq property[:access_network] }
 end
 
 describe network_fact('eth1') do
-	its(:cidr) 	{ should eq "#{property[:backend_network]}" }
+	its(:cidr) 	{ should eq property[:backend_network] }
 end
 
